3af9f2442a3b9cdb01468f36fc9e2caeaf684aa3
Yeikzy <48528776+Yeikzy@users.noreply.github.com>
2022-01-24 20:50:34 +0100

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,23 @@
+const config = {
+    
+    USERNAME: "Yeikzy#0001",
+    PICTURE: "assets/img/profile_picture.png",
+
+    DESCRIPTION: "Back-end Developer from France working with JavaScript / Python. Open source contributor.",
+
+    BADGES: [
+        { id: "HypeSquad Brillance", image: "assets/img/hypesquad_brillance.svg" },
+        { id: "Nitro Sub", image: "assets/img/nitro_sub.svg" },
+    ],
+
+    PROJECTS: [
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.gg/QW4fSZJW4N", name: "Yeikzy" },
+        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.gg/tendo", name: "Yloh" },
+    ],
+
+    SOCIALS: [
+        { class_name: "fab fa-github", link: "https://github.com/yeikzy" },
+        { class_name: "fab fa-twitter", link: "https://twitter.com/yeikzy" },
+    ],
+
+}
