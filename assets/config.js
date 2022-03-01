66c794aaba8d11c47e05780d868e5523516e0c07
Yeikzy <yeikzypro@gmail.com>
2022-03-01 20:31:56 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -15,6 +15,8 @@ const config = {
         { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.gg/ErwAkKhMsR", name: "Yeikzy" },
         { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.gg/CvmS8STsbM", name: "osu! France" },
         { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.gg/tendo", name: "Yloh" },
+        { id: "project-4", image: "assets/img/placeholder4.png", link: "https://discord.gg/6zmKxMgveS", name: "Verse" },
+        { id: "project-5", image: "assets/img/placeholder5.png", link: "https://discord.gg/6zmKxMgveS", name: "Arobase" },
     ],
 
     SOCIALS: [
